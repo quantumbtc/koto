aee844adbded80aa95bd8d712619c3749c84f2a5
WO <wo@kotocoin.info>
2020-09-09 21:28:28 +0900

    Add -mmacosx-version-min=$(OSX_MIN_VERSION) for building curl on darwin

--- depends/packages/curl.mk
+++ depends/packages/curl.mk
@@ -8,6 +8,7 @@ $(package)_dependencies=openssl
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --with-ssl
   $(package)_cflags_aarch64_linux=-fPIC
+  $(package)_cflags_darwin=-mmacosx-version-min=$(OSX_MIN_VERSION)
 endef
 
 define $(package)_config_cmds
